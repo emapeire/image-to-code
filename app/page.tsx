50c3eee9e422196e43af596fe53ee602532f8c7a
Emanuel Peire <emapeire@outlook.com>
2023-12-31 17:14:39 -0300

    Update global CSS and add Button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
 	return (
 		<main className="">
 			<h2 className="">Hello world!</h2>
+			<Button>Click me</Button>
 		</main>
 	)
 }
