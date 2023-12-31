931c5720231c32ad19f0619aade51775181751e1
Emanuel Peire <emapeire@outlook.com>
2023-12-31 17:05:43 -0300

    Format code using Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return (
-    <main className=''>
-      <h2 className=''>Hello world!</h2>
-    </main>
-  )
+	return (
+		<main className="">
+			<h2 className="">Hello world!</h2>
+		</main>
+	)
 }
